d7316dd01230b1b6bd303f362dfce88005f90d9c
kotsolesya <kots.olesya@gmail.com>
2016-01-22 12:34:14 +0200

    created Player and testPlayer

---
+++ src/tst/testPlayer.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package tst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class testPlayer {
+
+
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
