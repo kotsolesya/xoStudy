6e52f237b3546d6b0e9f57160dd868132078b005
kotsolesya <kots.olesya@gmail.com>
2016-01-22 12:59:36 +0200

    v2 testPlayer

--- src/tst/testPlayer.java
+++ src/tst/testPlayer.java
@@ -9,16 +9,36 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import myJava.xo.Figure;
+import myJava.xo.Player;
+
 
 public class testPlayer {
 
 	@Test
 	public void testGetName()  throws Exception {
-	
+		final String inputValue = "Slava";
+		final String expectedValue = inputValue;
+		
+		final Player player= new Player(inputValue, null);
+		
+		final String actualValue = player.getName();
+		
+		assertEquals(expectedValue, actualValue);
 	}
 	
 	@Test
 	public void testGetFigure()  throws Exception {
 	
+		final Figure inputValue = Figure.X;
+		final Figure expectedValue = inputValue;
+		
+		final Player player= new Player(null, inputValue);
+		
+		final Figure actualValue = player.getFigure();
+		
+		assertEquals(expectedValue, actualValue);
+		
+		
 	}
 }
