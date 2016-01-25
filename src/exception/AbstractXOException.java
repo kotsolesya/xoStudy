d59f3c2d6b5d7331c8e4d8cd65f5885f3f4bb3a2
kotsolesya <kots.olesya@gmail.com>
2016-01-25 15:55:39 +0200

    AbstractXOException & InvalidePointException add

---
+++ src/exception/AbstractXOException.java
@@ -0,0 +1,5 @@
+package exception;
+
+public class AbstractXOException extends Exception{
+
+}
---
+++ src/exception/InvalidePointException.java
@@ -0,0 +1,5 @@
+package exception;
+
+public class InvalidePointException  extends AbstractXOException{
+
+}
