7f17d9340939ff1f9d767a129a7aeefb30d23cca
kotsolesya <kots.olesya@gmail.com>
2016-01-25 16:05:16 +0200

    Field whith Exception 2 day

--- src/myJava/xo/Field.java
+++ src/myJava/xo/Field.java
@@ -2,8 +2,13 @@ package myJava.xo;
 
 import java.awt.Point;
 
+import exception.AlreadyOccupiedException;
+import exception.InvalidePointException;
+
 public class Field {
 	private static final int FIELD_SIZE = 3;
+	private static final int MIN_COORDINATE = 0;
+	private static final int MAX_COORDINATE = FIELD_SIZE;
 		
 	private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -11,11 +16,31 @@ public class Field {
 		return FIELD_SIZE;
 	}
 	
-	public Figure getFigure(final Point point){
+	public Figure getFigure(final Point point) throws InvalidePointException{
+		if (!checkPoint(point)){
+			throw new InvalidePointException();
+		}
 		return field[point.x][point.y];
     }
 	
-	public void setFigure(final Point point, final Figure figure){
+	public void setFigure(final Point point, final Figure figure) throws InvalidePointException, AlreadyOccupiedException{
+		if (!checkPoint(point)){
+			throw new InvalidePointException();
+		}
+		
+		if (field[point.x][point.y] !=null){
+			throw new AlreadyOccupiedException();
+		}
 		field[point.x][point.y]= figure;
 	}
+	
+	private boolean checkPoint(final Point point){
+		return checkCoordinate(point.x)&& checkCoordinate(point.y);
+	}
+	
+	private boolean checkCoordinate(final int coordinate){
+		return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+		
+	}
+	
 }
