c4408f9392eacff7b8fac09cf9c677518b77f5e8
kotsolesya <kots.olesya@gmail.com>
2016-01-26 12:15:37 +0200

    add new test fo X and Y

--- src/tst/FieldTest.java
+++ src/tst/FieldTest.java
@@ -2,12 +2,15 @@ package tst;
 
 import static org.junit.Assert.*;
 
+
 import java.awt.Point;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import exception.AlreadyOccupiedException;
+import exception.InvalidPointException;
 import myJava.xo.Field;
 import myJava.xo.Figure;
 
@@ -30,5 +33,75 @@ public class FieldTest {
 		
 		assertEquals(inputFigure, actualFigure);
 	}
+	
+	@Test
+	public void testGetFigureWhenFigureIsNotSet() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0,0);
+		
+		final Figure actualFigure = field.getFigure(inputPoint);
+		
+		assertNull(actualFigure);
+	}
+	//координаты меньше нуля
+	@Test
+	public void testGetFigureWhenXIsLessThenZero() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(-1,0);
+		
+		try {
+		field.getFigure(inputPoint);
+		fail();
+		} catch (final InvalidPointException e) {}
+	}
+	
+	@Test
+	public void testGetFigureWhenYIsLessThenZero() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0,-1);
+		
+		try {
+		field.getFigure(inputPoint);
+		fail();
+		} catch (final InvalidPointException e) {}
+	}
+//координаты больше размера поля
+	@Test
+	public void testGetFigureWhenXIsMoreThenSize() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(field.getSize()+1,0);
+		
+		try {
+		field.getFigure(inputPoint);
+		fail();
+		} catch (final InvalidPointException e) {}
+	}
+	
+	@Test
+	public void testGetFigureWhenYIsMoreThenSize() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0,field.getSize()+1);
+		
+		try {
+		field.getFigure(inputPoint);
+		fail();
+		} catch (final InvalidPointException e) {}
+	}
+
+	
+	@Test
+	public void testSetFigureWhenAlreadyOccupied() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0,0);
+		final Figure inputFigure = Figure.O;
+		
+		field.setFigure(inputPoint, inputFigure);
+		
+		try {
+			field.setFigure(inputPoint,inputFigure);
+			fail();
+			} catch (final AlreadyOccupiedException e) {}
+	}
+	
 	
 }
